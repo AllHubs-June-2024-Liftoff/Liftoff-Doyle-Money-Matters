df630a30e58d9bec89b2d8aa5e3623028b9a0705
timwebdev1 <tlmcdermott79@gmail.com>
2024-12-18 21:20:05 -0600

    Replaced backend-dev commit with added build.gradle files

---
+++ mm_BackEnd/src/main/java/org/moneymatters/mm_BackEnd/MmBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.moneymatters.mm_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmBackEndApplication.class, args);
+	}
+
+}
---
+++ mm_BackEnd/src/test/java/org/moneymatters/mm_BackEnd/MmBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moneymatters.mm_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
